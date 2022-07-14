72a2927ffc97f99a2c12772dcb5976faa4208816
abdecd <3404561918@qq.com>
2022-07-14 16:46:39 +0800

    t

--- src/view/Lyric.jsx
+++ src/view/Lyric.jsx
@@ -5,19 +5,25 @@ import { useParams } from 'react-router-dom'
 export default function Lyric() {
     var {musicId} = useParams();
     const [lyric, setLyric] = useState("");
+    const [loading, setLoading] = useState(true);
 
     //fetch lyric
     useEffect(() => {
         (async () => {
+            //设置加载效果
+            setLoading(true);
+
             var obj = (await axios(`/api/song/lyric?os=pc&id=${musicId}&lv=-1&tv=-1`)).data;
             var lrcGot = obj.lrc.lyric;
             // var lrcGot = obj.tlyric.lyric;
             setLyric(lrcGot);
+
+            setLoading(false);
         })();
     },[]);
 
     return (
-        <div style={{textAlign: "center"}}>
+        <div style={{textAlign: "center", transition: "0.2s", opacity: (loading ? 0.35 : 1)}}>
             { lyric.replace(/\[[^\]]+\]/g,"|&|&|").split("|&|&|").map(elem => <p>{elem}</p>) }
         </div>
     )
