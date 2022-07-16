d0d42f50a1951d12e81d1227c041165b7dccdb4c
abdecd <3404561918@qq.com>
2022-07-16 20:37:25 +0800

    t

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -10,6 +10,8 @@ export default function App({children}) {
             <div style={{height: "calc(100vh - 60px)", overflow: "auto"}}>
                 {children}
             </div>
+
+            {loopBlockShown && <div style={{position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh"}} onClick={() => setLoopBlockShown(false)}></div>}
             <div style={{
                 transition: "0.3s",
                 opacity: (loopBlockShown ? 1 : 0),
@@ -20,6 +22,7 @@ export default function App({children}) {
             }}>
                 <LoopBlock/>
             </div>
+            
             <MusicBar toggleLoopBlockShown={() => setLoopBlockShown(!loopBlockShown)}/>
         </div>
     )
