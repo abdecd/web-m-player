7515ee42e1143f11006f1411634f06add9ab83e1
abdecd <3404561918@qq.com>
2022-08-18 22:51:39 +0800

    t

--- src/js/WebMusicList.js
+++ src/js/WebMusicList.js
@@ -1,23 +1,23 @@
 import Subscription from './Subscription'
 import webMusicListStorage from './webMusicListStorage';
 
-class BasicWebMusicList extends Array {
+class BasicWebMusicList {
     index = -1;
-    
-    constructor() { super(); }
+    arr = [];
+    randomList = [];
 
-    next() { return this.length==0 ? null : this[this.index = (this.index>=this.length-1 ? 0 : this.index+1)]; }
-    before() { return this.length==0 ? null : this[this.index = (this.index<=0 ? (this.length-1) : this.index-1)]; }
+    next() { return this.arr.length==0 ? null : this.arr[this.index = (this.index>=this.arr.length-1 ? 0 : this.index+1)]; }
+    before() { return this.arr.length==0 ? null : this.arr[this.index = (this.index<=0 ? (this.arr.length-1) : this.index-1)]; }
     nextRandom() {
-        if (this.length==0) return null;
+        if (this.arr.length==0) return null;
         //create new list
-        if (!this.randomList || this.randomList.length==0) {
-            this.randomList = new Array(this.length);
-            for (let i=0;i<this.length;i++) this.randomList[i]=i;
+        if (this.randomList.length==0) {
+            this.randomList = new Array(this.arr.length);
+            for (let i=0;i<this.arr.length;i++) this.randomList[i]=i;
         }
         
         //get an element from randomList, delete and return it
-        return this[this.index = this.randomList.splice(Math.floor(Math.random()*this.randomList.length),1)[0]];
+        return this.arr[this.index = this.randomList.splice(Math.floor(Math.random()*this.randomList.length),1)[0]];
     }
 }
 
@@ -28,50 +28,50 @@ class WebMusicList extends BasicWebMusicList {
     storage = false;
     changeSub = new Subscription();
 
+    get length() { return this.arr.length; }
+
     constructor(name="defaultList",arr=null,storage=false) {
         super();
         this.name = name || "defaultList";
         this.storage = storage;
-        if (arr) for (let i=0,L=arr.length;i<L;i++) if (WebMusicList.isValidItem(arr[i])) this[i]=arr[i];
-        if (this.storage) webMusicListStorage.set(this.name,this);
+        if (arr) for (let i=0,L=arr.length;i<L;i++) if (WebMusicList.isValidItem(arr[i])) this.arr[i]=arr[i];
+        if (this.storage) webMusicListStorage.set(this.name,this.arr);
     }
 
-    static isValidItem(obj) { return obj.name && (obj.src || obj.id); }
+    static isValidItem(obj) { return obj && obj.name && (obj.src || obj.id); }
     static getIdOrSrc(elem) { return elem.id || elem.src; }
     
     push(obj) {
         if (!WebMusicList.isValidItem(obj)) return false;
-        super.push(obj);
+        this.arr.push(obj);
         this.randomList = null;
-        this.changeSub.publish(new WebMusicList(this.name,this,false));
-        if (this.storage) webMusicListStorage.set(this.name,this);
+        this.changeSub.publish(new WebMusicList(this.name,this.arr,false));
+        if (this.storage) webMusicListStorage.set(this.name,this.arr);
         return true;
     }
     pop() {
-        var ans = super.pop();
+        var ans = this.arr.pop();
         this.randomList = null;
-        this.changeSub.publish(new WebMusicList(this.name,this,false));
-        if (this.storage) webMusicListStorage.set(this.name,this);
+        this.changeSub.publish(new WebMusicList(this.name,this.arr,false));
+        if (this.storage) webMusicListStorage.set(this.name,this.arr);
         return ans;
     }
-    shift(...sth) { throw Error("should not use it."); }
-    unshift(...sth) { throw Error("should not use it."); }
     splice(deleteIndex,wantDeleteCnt) {
         var deleteCnt = (deleteIndex<=this.index) ?
             Math.min(this.index-deleteIndex+1,wantDeleteCnt)
             : 0;
         this.index-=deleteCnt;
         
-        var ans = super.splice(deleteIndex,wantDeleteCnt);
+        var ans = this.arr.splice(deleteIndex,wantDeleteCnt);
         this.randomList = null;
-        this.changeSub.publish(new WebMusicList(this.name,this,false));
-        if (this.storage) webMusicListStorage.set(this.name,this);
+        this.changeSub.publish(new WebMusicList(this.name,this.arr,false));
+        if (this.storage) webMusicListStorage.set(this.name,this.arr);
         return ans;
     }
 
     search(idOrSrc) {
-        for (let i=0,L=this.length;i<L;i++) {
-            if (WebMusicList.getIdOrSrc(this[i])==idOrSrc) return i;
+        for (let i=0,L=this.arr.length;i<L;i++) {
+            if (WebMusicList.getIdOrSrc(this.arr[i])==idOrSrc) return i;
         }
         return -1;
     }
@@ -80,13 +80,13 @@ class WebMusicList extends BasicWebMusicList {
         var oldIndex = 0, newIndex = this.search(idOrSrc);
         if (newIndex==-1) return false;
         
-        var swap = this[oldIndex];
-        this[oldIndex] = this[newIndex];
-        this[newIndex] = swap;
+        var swap = this.arr[oldIndex];
+        this.arr[oldIndex] = this.arr[newIndex];
+        this.arr[newIndex] = swap;
 
         this.randomList = null;
-        this.changeSub.publish(new WebMusicList(this.name,this,false));
-        if (this.storage) webMusicListStorage.set(this.name,this);
+        this.changeSub.publish(new WebMusicList(this.name,this.arr,false));
+        if (this.storage) webMusicListStorage.set(this.name,this.arr);
         return true;
     }
 
