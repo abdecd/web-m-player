c29cb88238888367fb99b54ba98f564d6a093765
abdecd <null>
2022-11-05 23:45:18 +0800

    t

--- src/component/LoopBlock.jsx
+++ src/component/LoopBlock.jsx
@@ -132,7 +132,7 @@ function BasicLoopBlock() {
 
     var copyList = useCallback((ev,elem) => {
         var newListName=elem.name+" copy";
-        while (webMusicListStorage.names.includes(newListName)) newListName+="_";
+        while (webMusicListStorage.names.includes(newListName)) newListName+="I";
         new WebMusicList(newListName,webMusicListStorage.get(elem.name),true);
         showTips.info("复制成功。");
     },[]);
