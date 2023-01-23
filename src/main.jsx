b42c2c83da4ca589f4772c1e39ec61495458be2e
abdecd <null>
2023-01-23 14:08:28 +0800

    更改polyfill注入时机

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ApplyPolyfills from './js/applyPolyfills'
-ApplyPolyfills();
 import MainRouter from './router/MainRouter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
