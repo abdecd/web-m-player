bead12f1e25aadcffb68627298acff2ba6acfbcb
abdecd <null>
2023-02-25 00:38:06 +0800

    修复icon颜色问题

--- src/view/LoopBlock/MusicList.jsx
+++ src/view/LoopBlock/MusicList.jsx
@@ -157,7 +157,7 @@ function EditList({listData,currentIndex,setIsEditing,isFiltered,undoSpecificLis
                         <RightBtn
                             className="holder"
                             disableRipple
-                            btnText={<svg style={{position: "relative",left:"-4px",width: "1em",height: "1em",verticalAlign: "middle",fill: "currentColor",overflow: "hidden"}} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5826"><path d="M64.1 194v89.6h896.1V194H64.1z m0 358.4h896.1v-89.6H64.1v89.6z m0 268.9h896.1v-89.6H64.1v89.6z" fill="#383838" p-id="5827"></path></svg>}
+                            btnText={<svg style={{position: "relative",left:"-4px",width: "1em",height: "1em",verticalAlign: "middle",fill: "currentColor",overflow: "hidden"}} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1980"><path d="M64.1 194v89.6h896.1V194H64.1z m0 358.4h896.1v-89.6H64.1v89.6z m0 268.9h896.1v-89.6H64.1v89.6z" p-id="1981"></path></svg>}
                             style={{flexBasis: "40px"}}/>
                     </ListItem>
                 ))}
