af8a63d60cc95c45e87b5bfdec405f18c7a1151d
abdecd <null>
2023-03-20 21:18:21 +0800

    t

--- src/component/ListnameListPopup.jsx
+++ src/component/ListnameListPopup.jsx
@@ -16,7 +16,7 @@ export default function ListnameListPopup({shown,setShown,titleBlock,onItemClick
         return () => webMusicListStorage.removeNamesChangeListener(refreshFn);
     },[]);
     
-    const delayTime = 200;
+    const delayTime = 300;
     var { exist, shouldRender } = useRenderTimeRemainer(shown,delayTime);
     const zIndex = 2;
     var divStyle = {
@@ -29,8 +29,8 @@ export default function ListnameListPopup({shown,setShown,titleBlock,onItemClick
         width: "80vw",
         height: "70vh",
         borderRadius: "5px",
-        backdropFilter: "blur(6px) brightness(1.2)",
-        boxShadow: "0px 0px 20px 0px rgba(0,0,0,0.2)",
+        backdropFilter: "blur(12px) brightness(2)",
+        boxShadow: "0px 0px 20px 0px rgba(0,0,0,0.5)",
         display: "flex",
         flexDirection: "column",
 
@@ -43,7 +43,7 @@ export default function ListnameListPopup({shown,setShown,titleBlock,onItemClick
         left: 0,
         width: "100vw",
         height: "100vh",
-        background: "rgba(0,0,0,0.2)",
+        background: "rgba(0,0,0,0.5)",
         opacity: (shouldRender ? 1 : 0),
         transition: delayTime/1000+"s",
         zIndex
