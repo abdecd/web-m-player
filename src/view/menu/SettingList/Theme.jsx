622410c195e05b2ff5031a172e136fc58e12eebd
abdecd <null>
2023-07-12 19:33:22 +0800

    t

--- src/view/menu/SettingList/Theme.jsx
+++ src/view/menu/SettingList/Theme.jsx
@@ -3,18 +3,18 @@ import ListItemText from '@mui/material/ListItemText';
 
 import { settingsStorage } from '../../../js/settings';
 import { ListItem, Switch } from '@mui/material';
-import theme from '../../../js/themeManager';
+import themeManager from '../../../js/themeManager';
 
 export default function ThemeSettingItem() {
-    const [isDarkMode, setIsDarkMode] = useState(theme.getThemeType()=="dark");
+    const [isDarkMode, setIsDarkMode] = useState(themeManager.getThemeType()=="dark");
 
     var handleChange = useCallback(checked => {
         if (checked) {
-            theme.setThemeType("dark");
+            themeManager.setThemeType("dark");
             setIsDarkMode(true);
             settingsStorage.setSetting("isDarkMode",true);
         } else {
-            theme.setThemeType("light");
+            themeManager.setThemeType("light");
             setIsDarkMode(false);
             settingsStorage.setSetting("isDarkMode",false);
         }
