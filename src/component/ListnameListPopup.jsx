e31a20c1adb874bb2d5acc535571d2d30ac120c5
abdecd <null>
2023-07-14 22:42:48 +0800

    修复Popup在win chrome上模糊的问题

--- src/component/ListnameListPopup.jsx
+++ src/component/ListnameListPopup.jsx
@@ -4,6 +4,46 @@ import BehindRoot from "../component/BehindRoot";
 import useRenderTimeRemainer from "../js/reactHooks/useRenderTimeRemainer";
 import webMusicListStorage from "../js/webMusicListStorage";
 import { LeftItem } from "./ListButton";
+import { styled } from "styled-components";
+
+const CenterBlock = styled.div`
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    width: 80vw;
+    height: 70vh;
+    border-radius: 5px;
+`
+
+const StyledPopup = styled(CenterBlock)`
+    opacity: ${p=>p.$opacity};
+    transition: ${p=>p.$delayTime/1000}s;
+    z-index: ${p=>p.$zIndex};
+    box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.5);
+    display: flex;
+    flex-direction: column;
+`
+
+const StyledBg = styled(CenterBlock)`
+    z-index: ${p=>p.$zIndex};
+    opacity: ${p=>p.$opacity};
+    transition: ${p=>p.$delayTime/1000}s;
+    backdrop-filter: blur(12px) brightness(2);
+    -webkit-backdrop-filter: blur(12px) brightness(2);
+`
+
+const Mask = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0,0,0,0.5);
+    z-index: ${p=>p.$zIndex};
+    opacity: ${p=>p.$opacity};
+    transition: ${p=>p.$delayTime/1000}s;
+`
 
 export default function ListnameListPopup({shown,setShown,titleBlock,onItemClick}) {
     const [nameList, setNameList] = useState([]);
@@ -16,39 +56,22 @@ export default function ListnameListPopup({shown,setShown,titleBlock,onItemClick
     },[exist]);
     
     const zIndex = 2;
-    var divStyle = {
-        position: "absolute",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%,-50%)",
-        zIndex,
-
-        width: "80vw",
-        height: "70vh",
-        borderRadius: "5px",
-        backdropFilter: "blur(12px) brightness(2)",// todo: chrome模糊
-        boxShadow: "0px 0px 20px 0px rgba(0,0,0,0.5)",
-        display: "flex",
-        flexDirection: "column",
-
-        opacity: shouldRender ? 1 : 0,
-        transition: delayTime/1000+"s",
-    };
-    var maskStyle = {
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        background: "rgba(0,0,0,0.5)",
-        opacity: (shouldRender ? 1 : 0),
-        transition: delayTime/1000+"s",
-        zIndex
-    };
 
     return <BehindRoot>{exist && <>
-        <div style={maskStyle} onClick={() => setShown(false)}/>
-        <div style={divStyle}>
+        <Mask
+            $zIndex={zIndex}
+            $opacity={shouldRender ? 1 : 0}
+            $delayTime={delayTime}
+            onClick={() => setShown(false)}/>
+        <StyledBg
+            $zIndex={zIndex}
+            $opacity={shouldRender ? 1 : 0}
+            $delayTime={delayTime}/>
+        <StyledPopup
+            $zIndex={zIndex}
+            $opacity={shouldRender ? 1 : 0}
+            $delayTime={delayTime}
+        >
             {titleBlock}
             <List style={{ flex: "1 1 0", overflow: "auto" }}>
             { nameList.map(x =>
@@ -58,6 +81,6 @@ export default function ListnameListPopup({shown,setShown,titleBlock,onItemClick
                     clickFn={() => onItemClick(x.name)}/>
             ) }
             </List>
-        </div>
+        </StyledPopup>
     </>}</BehindRoot>
 }
