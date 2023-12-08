209748ad8312098f583b94208f91e3faf7c86b98
abdecd <null>
2023-12-08 16:21:24 +0800

    简化longclick

--- src/js/click/bindLongClick.js
+++ src/js/click/bindLongClick.js
@@ -17,86 +17,35 @@ var os = (function () {
 })();
 
 function bindLongClick(target, clickFn=(function(){}), longClickFn=(function(){})) {
-    const longTime = 400;
-    var timer = null;
-    var startX = -1, startY = -1; // moved: -1
-    var posHandler = new PosHandler(target);
+    var timer = null, longTime = 400;
 
     var startFn = ev => {
-        [startX, startY] = posHandler.getPosFromEvent(ev);
-        posHandler.addSingleMoveFn(pos => {
-            if (startX != pos[0] || startY != pos[1]) {
-                startX = -1, startY = -1;
-                posHandler.removeMoveFn();
-            }
-        });
-        timer = setTimeout(() => {
-            timer = null;
-            if (startX == -1 || startY == -1) return;
-            posHandler.removeMoveFn();
-            longClickFn(ev);
-        }, longTime);
+        timer = setTimeout(() => { longClickFn(ev); timer = null }, longTime);
+        if (os=="pc") {
+            target.addEventListener("mousemove",moveFn,{once: true, passive: true});
+        } else {
+            target.addEventListener("touchmove",moveFn,{once: true, passive: true});
+        }
     };
-    
+    var moveFn = ev => {
+        clearTimeout(timer);
+        timer = null;
+    };
     var endFn = ev => {
         if (timer) {
             clearTimeout(timer);
             timer = null;
-            if (startX == -1 || startY == -1) return;
-            posHandler.removeMoveFn();
             clickFn(ev);
         }
     };
 
-    // 保证重复调用时覆盖原先设置
     if (os=="pc") {
-        posHandler.clearMoveFn();
         target.onmousedown = startFn;
         target.onmouseup = endFn;
     } else {
-        posHandler.clearMoveFn();
         target.ontouchstart = startFn;
         target.ontouchend = endFn;
     }
 }
-
-class PosHandler {
-    constructor(target) {
-        this.target = target;
-    }
-    getPosFromEvent(ev) {
-        if (os=="pc") {
-            return [ev.clientX, ev.clientY];
-        } else {
-            return [ev.targetTouches[0].clientX, ev.targetTouches[0].clientY];
-        }
-    }
-    moveFnCb;
-    moveFn = ev => {
-        this.moveFnCb(this.getPosFromEvent(ev));
-    }
-    addSingleMoveFn(moveFnCb) {
-        this.moveFnCb = moveFnCb;
-        if (os=="pc") {
-            this.target.addEventListener("mousemove",this.moveFn);
-        } else {
-            this.target.addEventListener("touchmove",this.moveFn);
-        }
-    }
-    removeMoveFn() {
-        if (os=="pc") {
-            this.target.removeEventListener("mousemove",this.moveFn);
-        } else {
-            this.target.removeEventListener("touchmove",this.moveFn);
-        }
-    }
-    clearMoveFn() {
-        if (os=="pc") {
-            this.target.onmousemove = "";
-        } else {
-            this.target.ontouchmove = "";
-        }
-    }
-};
 
 export default bindLongClick;
