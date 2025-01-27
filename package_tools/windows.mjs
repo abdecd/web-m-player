3d547b160b9f1dd10f07e330c38fcdbb1afd6c70
abdecd <abdecd@users.noreply.github.com>
2025-01-27 19:13:24 +0800

    fix

--- package_tools/windows.mjs
+++ package_tools/windows.mjs
@@ -7,4 +7,6 @@ txt = txt
     .replaceAll(`.then(x => x.json())`,`.then(x => JSON.parse(x))`)
     .replaceAll(`fetchWithT(\`/api`,`fetchWithT(\`http://music.163.com/api`)
     .replaceAll(`fetchWithT("/discover`,`fetchWithT("https://music.163.com/discover`)
+    .replaceAll(`window.PhoneMusicManager?.getLocalListAbsolutePath()`, `window['go']['main']['App']['GetLocalListAbsolutePath']()`)
+    .replaceAll(`window.PhoneMusicManager?.loadFullList()`, `window['go']['main']['App']['LoadFullList']()`)
 fs.writeFileSync("./src/js/nativeBridge/musicAjax.js",txt)
