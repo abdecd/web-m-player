9cdadf30d4ce626f87a3c5903becfd33aff2e53c
abdecd <abdecd@users.noreply.github.com>
2025-01-27 15:37:15 +0800

    to wails

--- package_tools/windows.mjs
+++ package_tools/windows.mjs
@@ -1,6 +1,10 @@
 import fs from "fs"
 
 var txt = fs.readFileSync("./src/js/nativeBridge/musicAjax.js").toString()
-txt = txt.replaceAll(`fetchWithT(\`/api`,`fetchWithT(\`http://music.163.com/api`)
-.replaceAll(`fetchWithT("/discover`,`fetchWithT("https://music.163.com/discover`)
+txt = txt
+    .replaceAll(`import fetchWithT from "../utils/fetchWithT";`,`function fetchWithT(url,options){return window['go']['main']['App']['Fetch'](url)}`)
+    .replaceAll(`.then(x => x.text())`,``)
+    .replaceAll(`.then(x => x.json())`,`.then(x => JSON.parse(x))`)
+    .replaceAll(`fetchWithT(\`/api`,`fetchWithT(\`http://music.163.com/api`)
+    .replaceAll(`fetchWithT("/discover`,`fetchWithT("https://music.163.com/discover`)
 fs.writeFileSync("./src/js/nativeBridge/musicAjax.js",txt)
